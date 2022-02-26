a87792d518d7eacc0ab38aae73d1f4c08171a27b
Rakienyu <rv2226@nyu.edu>
2022-02-26 16:10:23 -0600

    tls commit

--- solution.py
+++ solution.py
@@ -5,10 +5,10 @@ import time
 msg = "\r\n I love Computer Networks"
 endmsg = "\r\n.\r\n"
 
-mailserver = ("mail.smtp2go.com", 2525) #Fill in start #Fill in end
+mailserver = ("mail.google.com", 2525) #Fill in start #Fill in end
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
-#clientSocket = ssl.wrap_socket(clientSocket)
+clientSocket = ssl.wrap_socket(clientSocket)
 clientSocket.connect(mailserver)
 
 recv = clientSocket.recv(1024)
@@ -25,33 +25,33 @@ print("Message after EHLO command:" + recv1)
 if recv1[:3] != '250':
     print('250 reply not received from server.')
 
-#command = 'STARTTLS \r\n'
-#command = command.encode()
-#clientSocket.send(command)
-#recv2 = clientSocket.recv(1024)
-#recv2 = recv2.decode()
-#print("Message after STARTTLS command:" + recv2)
-#if recv2[:3] != '250':
-#    print('250 reply not received from server.')
+command = 'STARTTLS \r\n'
+command = command.encode()
+clientSocket.send(command)
+recv2 = clientSocket.recv(1024)
+recv2 = recv2.decode()
+print("Message after STARTTLS command:" + recv2)
+if recv2[:3] != '250':
+    print('250 reply not received from server.')
 
 
-#Info for username and password
-#username =  "rjcool.joshi2@gmail.com" 	#input("Enter the username : ") 
-#password =  "uk0GEj4BWSh2" 				#input("Enter the password : ") 
-#base64_str = ("\x00"+username+"\x00"+password).encode()
-#base64_str = base64.b64encode(base64_str)
-#authMsg = "AUTH PLAIN ".encode()+base64_str+"\r\n".encode()
-#clientSocket.send(authMsg)
-#recv_auth = clientSocket.recv(1024)
-#print(recv_auth.decode())
+Info for username and password
+username =  "rjcool.joshi2@gmail.com" 	#input("Enter the username : ") 
+password =  "uk0GEj4BWSh2" 				#input("Enter the password : ") 
+base64_str = ("\x00"+username+"\x00"+password).encode()
+base64_str = base64.b64encode(base64_str)
+authMsg = "AUTH PLAIN ".encode()+base64_str+"\r\n".encode()
+clientSocket.send(authMsg)
+recv_auth = clientSocket.recv(1024)
+print(recv_auth.decode())
 
-mailFrom = "MAIL FROM: <rjcool.joshi2@gmail.com> \r\n"
+mailFrom = "MAIL FROM: <rakesh.vanam99@gmail.com> \r\n"
 clientSocket.send(mailFrom.encode())
 recv2 = clientSocket.recv(1024)
 recv2 = recv2.decode()
 print("After MAIL FROM command: "+recv2)
 
-rcptTo = "RCPT TO: <rj1234@nyu.edu> \r\n"
+rcptTo = "RCPT TO: <vanam.rakesh@gmail.com> \r\n"
 clientSocket.send(rcptTo.encode())
 recv3 = clientSocket.recv(1024)
 recv3 = recv3.decode()
