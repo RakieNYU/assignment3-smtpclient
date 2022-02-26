8c7f7d7760f7299960be29bc1f0deb6c39ba1e46
Rakienyu <rv2226@nyu.edu>
2022-02-26 16:25:31 -0600

    test2

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 # RakieNYU
 from socket import *
 
-def smtp_client(port=1025, mailserver=('127.0.0.1', 1025)):
+def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     msg = "\r\n SMTP LAB"
     endmsg = "\r\n.\r\n"
